3b455687f2dd47d3821272ded100eafaa474fcb1
zachhelfand <zachhelfand@josiecalsiphone.localdomain>
2023-02-09 15:09:44 -0600

    Studio 3 Completed

---
+++ src/studio3/Sieve.java
@@ -0,0 +1,30 @@
+package studio3;
+
+import java.util.Scanner;
+
+public class Sieve {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("n (inclusive) = ");
+		int n = in.nextInt();
+		boolean[] array = new boolean[n];
+		for (int j = 0; j < array.length; j++) {
+			if (j > 1) {
+				array[j] = true;
+			}
+		}
+		for (int i = 2; i < array.length/2; i++) {
+			for (int k = i*2; k < array.length; k+=i)	{
+				array[k] = false;
+			}
+			}
+		for (int num = 0; num < array.length; num++) {
+			if (array[num] == true) {
+				System.out.println(num);
+			}
+		}
+			}
+	}
+
+
